6c35a9679d477c730fa32ac4130d569cd0f73b0c
CotneTuxashvili <cotnetukhashvili53@gmail.com>
2024-05-28 20:07:59 +0400

    level 3

---
+++ level 3/homework/lesson3.py
@@ -0,0 +1,35 @@
+#things about myself
+
+name = "my name is cotne"
+
+surname = " my surname is tukhashvili"
+
+age = " im 12 years old"
+
+country = " im from georgia"
+
+hobby = " i like proggraming"
+
+print(name + surname + age + country + hobby)
+
+#math calculating
+
+print(13 + 34)
+
+print(54 - 24)
+
+print(143 * 234)
+
+print (12 // 3)
+
+print(174 ** 3)
+
+print(186 + 123)
+
+print(276 - 201)
+
+print(987 * 23)
+
+print(354 - 24)
+
+print(144 // 12)
