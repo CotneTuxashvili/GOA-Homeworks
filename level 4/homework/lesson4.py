4e40c73c3b77f0ac4798f3c78ff70fa5b2ba51d4
CotneTuxashvili <cotnetukhashvili53@gmail.com>
2024-06-05 18:34:27 +0400

    level 4

---
+++ level 4/homework/lesson4.py
@@ -0,0 +1,29 @@
+print(12 + 3)
+
+print(25 + 5)
+
+print(13 + 3)
+
+print(25 - 3)
+
+print(77 - 7)
+
+print(54 - 24)
+
+print(25 * 5)
+
+print(10 * 3)
+
+print(54 * 3)
+
+print(12 / 3)
+
+print(76 / 2)
+
+print(13 // 3)
+
+print(87 // 3)
+
+print(12 ** 3)
+
+print(14 ** 3)
